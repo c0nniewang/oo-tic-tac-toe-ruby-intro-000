897b9cacc4895feb636998be38783171a1bb7e4b
Connie Wang <conniewang2012@u.northwestern.edu>
2017-09-20 20:14:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,10 @@
 class TicTacToe
-  def initialize(board)
-    @board = board
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " "," ", " "]
   end
 
   def board=(board)
-    @board = [" ", " ", " ", " ", " ", " ", " "," ", " "]
+    @board = board
   end
 
   def board
